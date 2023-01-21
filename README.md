0156419c1bbf6d6c6c016805f19471a43368609a
Gustavo M. Minchiotti <Gustavominchiotti@gmail.com>
2023-01-21 16:23:32 -0300

    Entrega

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# Currículum Vitae
+# Trabajo final integrador
 
-## Trabajo final integrador
+## Currículum Vitae
 
-### Ticmas para argentina programa 4.0
+### Beca de argentina programa 4.0 a través de Ticmas
 
-En este proyecto trabajé con HTML5 CSS3 Bootstrap5 y algunas funcionalidades respecto al manejo del DOM con JavaScript
+-------------------------------------
+
+En este proyecto trabajé con **HTML5, CSS3, Bootstrap5** y algunas funcionalidades en **JavaScript** con manejo del **DOM**.
+
+La plantilla que utilicé para dar formato es de una clásica pagina de confección de CVs <https://resume-example.com/> y entre las opciones seleccioné el modelo **Rotterdam**
+
+Los datos son los de mi CV real ya que lo utilizaré como parte de mi porfolio.
--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-//mouseover es equivalente a hover
-
-//? de esta manera capture un color porque las variables sass ($oscuro) no son compatibles con js
-// document.getElementById("whaText").addEventListener("mouseout", function () {
-
-//   let elemento = document.getElementById("colorClaro");
-//   let elementSt = window.getComputedStyle(elemento);
-//   let elColor = elementSt.getPropertyValue("backgroundColor");
-//   console.log("elColor");
-
-//   document.getElementById("whatsappButton").style.backgroundColor = elColor;
-// });
-
-//* Return the background color of a specific <div> element:
-
-// let color = document.getElementById("myDiv").style.backgroundColor;
-
 //? capturo el bootstrap icon para cambiar el color de decorado ///////////////////
 
 document.getElementById("n1").addEventListener("click", function () {
@@ -77,6 +60,26 @@ document.getElementById("n3").addEventListener("click", function () {
   document.getElementById("hr5").style.borderColor = "#f2f2f2";
 });
 
+document.getElementById("n5").addEventListener("click", function () {
+  document.getElementById("colorOscuro").style.backgroundColor = "#2c2c2c";
+  document.getElementById("colorHeader").style.backgroundColor = "#2c2c2c";
+
+  //? fuentes..
+  document.getElementById("colorOscuro").style.color = "#f2f2f2";
+  document.getElementById("colorHeader").style.color = "#f2f2f2";
+
+  document.getElementById("whaText").style.color = "#f2f2f2";
+  document.getElementById("sobreText").style.color = "#f2f2f2";
+  document.getElementById("mapText").style.color = "#f2f2f2";
+  document.getElementById("linkText").style.color = "#f2f2f2";
+  document.getElementById("gitText").style.color = "#f2f2f2";
+  document.getElementById("hr1").style.borderColor = "#f2f2f2";
+  document.getElementById("hr2").style.borderColor = "#f2f2f2";
+  document.getElementById("hr3").style.borderColor = "#f2f2f2";
+  document.getElementById("hr4").style.borderColor = "#f2f2f2";
+  document.getElementById("hr5").style.borderColor = "#f2f2f2";
+});
+
 //! color naranja claro ------------------------////////////////
 document.getElementById("n4").addEventListener("click", function () {
   document.getElementById("colorOscuro").style.backgroundColor = "#FFD085";
@@ -98,10 +101,13 @@ document.getElementById("n4").addEventListener("click", function () {
   document.getElementById("hr5").style.borderColor = "#2c2c2c";
 });
 
-//? ///////////////////////////// toggle /////////////////////////////
+//mouseover es equivalente a hover
 
-const darkMode = document.getElementById("dark").addEventListener("click", function () {
-    console.log("darkMode");
-  });
-
-  
+//? de esta manera capture un color porque las variables sass ($oscuro) no son compatibles con js
+// document.getElementById("whaText").addEventListener("mouseout", function () {
+//   let elemento = document.getElementById("colorClaro");
+//   let elementSt = window.getComputedStyle(elemento);
+//   let elColor = elementSt.getPropertyValue("backgroundColor");
+//   console.log("elColor");
+//   document.getElementById("whatsappButton").style.backgroundColor = elColor;
+// });
