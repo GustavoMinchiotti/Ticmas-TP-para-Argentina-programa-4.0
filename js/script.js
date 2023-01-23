7372ecec72a07a353c7a4c631798f00ad0574c76
Gustavo M. Minchiotti <Gustavominchiotti@gmail.com>
2023-01-23 09:28:48 -0300

    Dark-light theme agregado

--- js/script.js
+++ js/script.js
@@ -111,3 +111,20 @@ document.getElementById("n4").addEventListener("click", function () {
 //   console.log("elColor");
 //   document.getElementById("whatsappButton").style.backgroundColor = elColor;
 // });
+
+
+//? ---------------------------light dark theme-----------------------------------
+function toggleTheme() { 
+    // Obtains an array of all <link> 
+    // elements. 
+    // Select your element using indexing. 
+    var theme = document.getElementsByTagName('link')[0]; 
+
+    // Change the value of href attribute  
+    // to change the css sheet. 
+    if (theme.getAttribute('href') == './css/style.css') { 
+        theme.setAttribute('href', './css/style-dark.css'); 
+    } else { 
+        theme.setAttribute('href', './css/style.css'); 
+    } 
+} 
